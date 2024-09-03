90a774fa0cf99ed46db2e3862c466ab6f0b34a11
margarita2104 <margarita.p.ferreira@gmail.com>
2024-09-03 14:04:53 +0200

    fixed issue with side bar opening with turbo, added questions and answer cards to the card collection form

--- app/models/card_collection.rb
+++ app/models/card_collection.rb
@@ -1,6 +1,6 @@
 class CardCollection < ApplicationRecord
-  has_many :cards
-
+  has_many :cards, dependent: :destroy
+  accepts_nested_attributes_for :cards, allow_destroy: true, reject_if: :all_blank
   validates :name, presence: true
   validates :description, presence: true
 end
