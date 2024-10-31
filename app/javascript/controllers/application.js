0c1743bbbdfd2c4ffc6e6d6f744a6a225cf47f06
margarita2104 <margarita.p.ferreira@gmail.com>
2024-10-31 09:13:12 +0100

    added sign up, log in, forgot password pages

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,15 @@
 import { Application } from "@hotwired/stimulus"
+import { start } from "@hotwired/turbo"
+import Rails from "@rails/ujs"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
-export { application }
+// Start Turbo
+start()
+
+// Start Rails UJS
+Rails.start()
