3f8bdcb2ac8604f6895628232cc7eb952d290a2e
margarita2104 <margarita.p.ferreira@gmail.com>
2024-11-25 20:25:39 +0100

    user sign up issue, kamall issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
   def name
     "#{first_name} #{last_name}"
   end
