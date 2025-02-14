7f0f1e3ebc63ba49a88c4944e7caf2154b0570a1
margarita2104 <margarita.p.ferreira@gmail.com>
2025-02-14 16:28:48 +0100

    gmail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,6 +104,11 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
+  config.action_mailer.default_url_options = {
+    host: 'swissitize.apps.rubymonstas.ch',
+    protocol: 'https'
+  }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
@@ -114,4 +119,7 @@ Rails.application.configure do
     authentication: 'plain',
     enable_starttls_auto: true
   }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
